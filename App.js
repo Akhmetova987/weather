b0b2a1181c9e4b3475ca93dcf7efc5eebaff0937
Akhmetova987 <43739379+Akhmetova987@users.noreply.github.com>
2019-07-14 12:50:38 +0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Info from "./components/info";
-import Form from "./components/form";
-import Weather from "./components/weather";
+import Info from "./components/Info";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
 const API_KEY = "ae59e4f61c9379bbc1f7fbdf9d510428";
 
@@ -12,7 +12,11 @@ class App extends React.Component{
         city: undefined,
         country: undefined,
         pressure: undefined,
+        sunrise: undefined,
         sunset: undefined,
+        humidity: undefined,
+        speed:undefined,
+        
         error: undefined
     }
 
@@ -25,19 +29,31 @@ class App extends React.Component{
         const api_url = await 
         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
         const data = await api_url.json();
+        console.log(data);
+
 
         var date = new Date(data.sys.sunset*1000);
         var hours = date.getHours(); 
         var minutes = "0" + date.getMinutes(); 
         var seconds = "0" + date.getSeconds();
         var sunset_date = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        
+        var date = new Date(data.sys.sunrise*1000);
+        var hours = date.getHours(); 
+        var minutes = "0" + date.getMinutes(); 
+        var seconds = "0" + date.getSeconds();
+        var sunrise_date = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
          this.setState({
             temp: data.main.temp,
             city: data.name,
             country: data.sys.country,
             pressure: data.main.pressure,
+            sunrise: sunrise_date,
             sunset: sunset_date,
+            humidity: data.main.humidity,
+            speed: data.wind.speed,
+            
             error: undefined
 
         });
@@ -48,28 +64,44 @@ class App extends React.Component{
             city: undefined,
             country: undefined,
             pressure: undefined,
+            sunrise: undefined,
             sunset: undefined,
+            humidity: undefined,
+            speed: undefined,
+           
             error: "Введите название города"
         });
     }
 }
 
 
-    render(){
+    render() {
         return (
-            <div>
-                <Info />
-                <Form weatherMethod={this.gettingWeather} />
-                <Weather 
-                   temp={this.state.temp}
-                   city={this.state.city}
-                   country={this.state.country}
-                   pressure={this.state.pressure}
-                   sunset={this.state.sunset}
-                   error={this.state.error}
-                
-                />
-
+            <div className="wrapper">
+                <div className="main">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-5 info">
+                            <Info />
+                         </div>
+                        <div className="col-sm-7 form">
+                            <Form weatherMethod={this.gettingWeather} />
+                            <Weather 
+                            temp={this.state.temp}
+                            city={this.state.city}
+                            country={this.state.country}
+                            pressure={this.state.pressure}
+                            sunrise={this.state.sunrise}
+                            sunset={this.state.sunset}
+                            humidity={this.state.humidity}
+                            speed={this.state.speed}
+                            
+                            error={this.state.error}
+                            />
+                        </div>
+                    </div>   
+                </div>
+                </div>
             </div> 
         );
     }
---
+++ Info.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Info = () => (
+    <div>
+                <h3>Погодное приложение.</h3>
+                <h1>Узнайте погоду в своем городе!</h1>
+            </div> 
+);
+export default Info;
